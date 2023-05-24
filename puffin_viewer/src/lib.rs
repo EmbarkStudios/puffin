1fd84074982d7e7598a536c925a9b92de8aaad2d
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-05-24 08:57:01 +0200

    Fix web build

--- puffin_viewer/src/lib.rs
+++ puffin_viewer/src/lib.rs
@@ -333,29 +333,4 @@ impl eframe::App for PuffinViewer {
 // When compiling for web:
 
 #[cfg(target_arch = "wasm32")]
-use eframe::wasm_bindgen::{self, prelude::*};
-
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen]
-pub struct WebHandle {
-    _handle: eframe::web::AppRunnerRef,
-}
-
-/// This is the entry-point for all the web-assembly.
-/// This is called once from the HTML.
-/// It loads the app, installs some callbacks, then returns.
-/// You can add more callbacks like this if you want to call in to your code.
-#[cfg(target_arch = "wasm32")]
-#[allow(clippy::unused_unit)]
-#[wasm_bindgen]
-pub async fn start(canvas_id: &str) -> Result<WebHandle, eframe::wasm_bindgen::JsValue> {
-    puffin::set_scopes_on(true); // quiet warning in `puffin_egui`.
-    let web_options = eframe::WebOptions::default();
-    eframe::start_web(
-        canvas_id,
-        web_options,
-        Box::new(|_cc| Box::new(PuffinViewer::new(Source::None))),
-    )
-    .await
-    .map(|handle| WebHandle { _handle: handle })
-}
+mod web;
