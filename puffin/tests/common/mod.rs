d6e760a779f9b92c09cfea4683e50933427843b5
Gwen Lg <me@gwenlg.fr>
2025-08-06 19:26:13 +0200

    add integration test of profile multiple frames

--- puffin/tests/common/mod.rs
+++ puffin/tests/common/mod.rs
@@ -1,4 +1,9 @@
-use std::{thread, time::Duration};
+#![allow(dead_code)]
+
+use std::{
+    thread::{self},
+    time::Duration,
+};
 
 pub fn process_1() {
     puffin::profile_function!();
@@ -14,4 +19,25 @@ fn sub_process_1_1() {
 fn sub_process_1_2() {
     puffin::profile_function!();
     thread::sleep(Duration::from_micros(2));
+}
+
+pub fn example_run() {
+    for idx in 0..4 {
+        puffin::profile_scope!("main", idx.to_string());
+
+        {
+            puffin::profile_scope!("sleep 1ms");
+            let sleep_duration = Duration::from_millis(1);
+            thread::sleep(sleep_duration);
+        }
+
+        {
+            puffin::profile_scope!("sleep 2ms");
+            let sleep_duration = Duration::from_millis(2);
+            thread::sleep(sleep_duration);
+        }
+        //println!("before new_frame {idx}");
+        puffin::GlobalProfiler::lock().new_frame();
+        //println!("after new_frame {idx}");
+    }
 }
