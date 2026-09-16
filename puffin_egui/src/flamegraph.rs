d6e6ecee3f65ba0165da3d23112e3afac08f5bbd
Isse <git@isse.rs>
2026-09-16 16:33:09 +0200

    dont use clip_rect_margin

--- puffin_egui/src/flamegraph.rs
+++ puffin_egui/src/flamegraph.rs
@@ -284,10 +284,8 @@ pub fn ui(
     });
 
     Frame::dark_canvas(ui.style()).show(ui, |ui| {
-        ui.visuals_mut().clip_rect_margin = 0.0;
-
         let available_height = ui.max_rect().bottom() - ui.min_rect().bottom();
-        ScrollArea::vertical().show(ui, |ui| {
+        ScrollArea::vertical().content_margin(0.0).show(ui, |ui| {
             let mut canvas = ui.available_rect_before_wrap();
             canvas.max.y = f32::INFINITY;
             let response = ui.interact(canvas, ui.id().with("canvas"), Sense::click_and_drag());
